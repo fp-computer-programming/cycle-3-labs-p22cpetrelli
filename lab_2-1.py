8ffb0f7c2d31b43975944c05f702d2f3bd570f9a
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2021-09-28 10:00:54 -0400

    add file

---
+++ lab_2-1.py
@@ -0,0 +1,7 @@
+# author
+
+x = input("Input an integer: ")
+
+if int(x) % 2 == 0:
+    print(x + " is an even number")
+
---
+++ lab_2-2.py
@@ -0,0 +1,10 @@
+# author
+
+x = input("Input an integer: ")
+
+if int(x) % 2 == 0:
+    print(x + " is an even number")
+else: 
+    print(x + " is an odd number")
+
+
