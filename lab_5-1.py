a1d767045e5c8548c3c8c671fcddaaf6f1f34329
Cam Petrelli <p22cpetrelli@fairfieldprep.org>
2021-10-04 09:56:59 -0400

    add file

---
+++ lab_5-1.py
@@ -0,0 +1,16 @@
+# author CJP 10/04/2021
+
+# 1 (name error)
+# int(a)
+
+# 2 (value error)
+# int('a')
+
+# 3 (type error)
+# 'a'+2
+
+# 4 (module not found)
+# import date
+
+# 5 (syntax error)
+# print("I'm a happy camper!)
